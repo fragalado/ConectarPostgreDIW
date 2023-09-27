2607b230fd5455216ffec322cc54fbcac9eb26cd
DESKTOP-V1IS21O\Puesto8 <frandanigd@gmail.com>
2023-09-27 13:50:56 +0200

    Agregar archivos de proyecto.

---
+++ ConectarPostgreCSharp/ConectarPostgreSQL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace ConectarPostgreCSharp
+{
+    class ConectarPostgreSQL
+    {
+        NpgsqlConnection conec = new NpgsqlConnection();
+
+        // Variables que tendran los valores de la base de datos
+        String servidor = "localhost";
+        String bd = "gestorBibliotecaPersonal";
+        String usuario = "postgres";
+        String pswd = "zepxuH-nyzfa5";
+        String puerto = "5432";
+
+        public NpgsqlConnection conectarBD()
+        {
+            String cadenaConexion = "Server=" + servidor + ";Port=" + puerto + ";User Id=" + usuario + ";Password=" + pswd + ";Database=" + bd + ";";
+
+            try
+            {
+                conec.ConnectionString = cadenaConexion;
+                conec.Open();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se ha podido conectar a la BD");
+            }
+
+            return conec;
+        }
+
+        public DataTable consultarBD(string query)
+        {
+            NpgsqlCommand comm = new NpgsqlCommand(query, conec);
+            NpgsqlDataAdapter datos = new NpgsqlDataAdapter(comm);
+            DataTable tabla = new DataTable();
+            datos.Fill(tabla);
+            return tabla;
+        }
+
+        public void desconectarBD()
+        {
+            try
+            {
+                conec.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No se ha podido cerrar la base de datos");
+            }
+        }
+        
+    }
+}
---
+++ ConectarPostgreCSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConectarPostgreCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inicializamos un objeto de tipo ConectarPostgreSQL
+            ConectarPostgreSQL conec = new ConectarPostgreSQL();
+
+            // Conectamos
+            conec.conectarBD();
+
+            // Consula de datos
+            DataTable tabla;
+            try
+            {
+                // Guardamos en tabla la DataTable que devuelve el metodo
+                tabla = conec.consultarBD("SELECT * FROM gbp_almacen.gbp_alm_cat_libros");
+
+                // Recorremos la DataTable para mostrar las columnas de cada fila
+                foreach (DataRow row in tabla.Rows)
+                {
+                    foreach (DataColumn columns in tabla.Columns)
+                    {
+                        Console.Write("{0}: {1}; ", columns.ColumnName, row[columns]);
+                    }
+                    Console.WriteLine(); // Para dejar un espacio
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR: No se ha podido hacer la consulta");
+            }
+
+            // Desconectamos
+            conec.desconectarBD();
+
+            // Para salir
+            Console.WriteLine("\n\n\tPulse una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
